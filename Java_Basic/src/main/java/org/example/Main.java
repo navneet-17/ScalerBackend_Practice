fab4eea6d3db462a25528c0afb97b72cc250458a
Navneet <navneetshree17@gmail.com>
2023-02-10 17:50:50 +0530

    adding Java Basic Files

---
+++ Java_Basic/src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0)
+            System.out.println(Lib.getGreeting());
+        else {
+            if (args[0].equals("example")) {
+                var httpLib = new HttpLib();
+                System.out.println(httpLib.getExampleDotCom());
+            }
+            if (args[0].equals("posts")) {
+
+                Retrofit retrofit = new Retrofit.Builder()
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .baseUrl("https://jsonplaceholder.typicode.com/")
+                        .build();
+
+                var api = retrofit.create(JSONPlaceholderAPI.class);
+
+                api.getPosts().execute().body().forEach(post -> {
+                    System.out.println(post.getTitle());
+                });
+            }
+            if (args[0].equals("photos")){
+                Retrofit retrofit = new Retrofit.Builder()
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .baseUrl("https://jsonplaceholder.typicode.com/")
+                        .build();
+
+                var api = retrofit.create(JSONPlaceholderAPI.class);
+
+                api.getPhotos().execute().body().forEach(photos -> {
+                    System.out.println(photos.getUrl());
+                });
+
+            }
+        }
+    }
+}
