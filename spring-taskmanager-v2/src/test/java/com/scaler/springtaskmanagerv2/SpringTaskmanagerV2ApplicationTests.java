93f438f80ea0fe1a11b07c5fba95e81c4882ae92
Navneet <navneetshree17@gmail.com>
2023-02-16 01:01:26 +0530

    TaskManager_Advanced-version1-Created the tables

---
+++ spring-taskmanager-v2/src/test/java/com/scaler/springtaskmanagerv2/SpringTaskmanagerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.springtaskmanagerv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTaskmanagerV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
